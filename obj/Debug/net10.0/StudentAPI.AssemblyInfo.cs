62d024d498c94a2eb9bebf8994a22f63423ac92d
AshrafGit256 <ashrafwalusimbi27@gmail.com>
2026-02-02 12:17:19 +0300

    Added my frontend

--- obj/Debug/net10.0/StudentAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/StudentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1229e386c34cd3c40fc5c7514dcc76063c896b24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff8ac6cd537ffa95c58297d704a7989f1207e2f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
